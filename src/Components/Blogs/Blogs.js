ffda3b6dfbf22a0531d0b8b23ada276563a6c78f
Dipankar Gupta <46720652+dipu528447@users.noreply.github.com>
2022-11-27 20:13:21 +0600

    blogs question 3 done

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -23,7 +23,16 @@ const Blogs = () => {
                     <p class="text-gray-700 text-base mb-4 text-start">
                        Ans: The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the Prototype of an object, we use Object.
                     </p>
-                    
+                </div>
+            </div>
+            <div class="flex justify-center mt-20">
+                <div class="block p-6 rounded-lg shadow-lg bg-white w-3/4">
+                    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2 text-start">Question 3: What is a unit test? Why should we write unit tests?</h5>
+                    <p class="text-gray-700 text-base mb-4 text-start">
+                       Ans: A  unit test is a way of testing a unit - the smallest piece of code that can be logically isolated in a system. 
+                       <br></br>
+                       The main objective of unit testing is to isolate written code to test and determine if it works as intended. Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages
+                    </p>
                 </div>
             </div>
         </div>
