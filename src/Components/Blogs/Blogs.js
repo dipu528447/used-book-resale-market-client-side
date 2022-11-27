7d453516067bd42510c2c4b0ee7d759caac58b94
Dipankar Gupta <46720652+dipu528447@users.noreply.github.com>
2022-11-27 20:10:18 +0600

    blogs question 2 done

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,23 +4,26 @@ const Blogs = () => {
     return (
         <div>
             <div class="flex justify-center mt-20">
-                <div class="block p-6 rounded-lg shadow-lg bg-white max-w-full">
-                    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2">What are the different ways to manage a state in a React application?</h5>
+                <div class="block p-6 rounded-lg shadow-lg bg-white w-3/4">
+                    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2 text-start">Question 1: What are the different ways to manage a state in a React application?</h5>
                     <p class="text-gray-700 text-base mb-4 text-start">
                        Ans: The Four Kinds of React State to Manage
                     </p>
-                    <ul className='text-start ml-20'>
+                    <ol className='text-start ml-20 list-decimal' >
                         <li>Local state.</li>
                         <li>Global state.</li>
                         <li>Server state.</li>
                         <li>URL state.</li>
-                    </ul>
-                        
-                        
-                        
-                        
+                    </ol>
+                </div>
+            </div>
+            <div class="flex justify-center mt-20">
+                <div class="block p-6 rounded-lg shadow-lg bg-white w-3/4">
+                    <h5 class="text-gray-900 text-xl leading-tight font-medium mb-2 text-start">Question 2: How does prototypical inheritance work?</h5>
+                    <p class="text-gray-700 text-base mb-4 text-start">
+                       Ans: The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the Prototype of an object, we use Object.
+                    </p>
                     
-                    <button type="button" class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
                 </div>
             </div>
         </div>
